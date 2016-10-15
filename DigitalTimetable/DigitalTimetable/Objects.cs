ee01bf972d0abdb420f21452da3f5cb3bacdd0bb
SapiensAnatis <jay0@me.com>
2016-10-15 20:10:53 +0100

    Add colour implementation (I think)

--- DigitalTimetable/DigitalTimetable/Objects.cs
+++ DigitalTimetable/DigitalTimetable/Objects.cs
@@ -9,41 +9,35 @@ namespace DigitalTimetable
 {
     class Subject
     {
-        // Foremost properties (most integral/obvious)
+        
 
-        private string _name; // backing field: we want custom behaviour on this accessor
+        private string _name; // backing field. See below for explanation.
         public string Name
         {
             get
             {
                 return _name; // With custom behaviour the {get;set;} shortcut is unavailable, which normally includes an (invisible) backing field.
+                // Without a backing field, attempting to access the current variable in a return statement will produce infinite recursion.
             }
             set
             {
                 _name = value; // update backing field
-                Abbreviation = value.GetSubjAbbreviation(); // Generate a new abbreviation automatically, reduces overhead in name changes
+                Abbreviation = value.GetSubjAbbreviation(); // Generate a new abbreviation automatically, saves hassle
             }
         }
 
         // Miscellanious properties
 
-        public string Abbreviation { get; private set; }
-        
-
+        public string Abbreviation { get; private set; } // Since we auto-generate the abbreviation, there should be no need to change it manually.
 
         public Subject(string n)
         {
-            Name = n;
-
+            Name = n; // Initialize properties.
         }
     }
 
     class Lesson : Subject // Extends subject class by adding times/dates
     {
-        public Lesson(string name, int period, int day, string where, string teacher) : base(name)
-        {
-            Period = period; Day = day; Location = where; Teacher = teacher;
-        }
 
         public int Period { get; set; }
         public int Day { get; set; }
@@ -51,9 +45,17 @@ namespace DigitalTimetable
         public string Location { get; set; }
         public string Teacher { get; set; }
 
-        public static SubjectUIElement GenerateUIElement()
+        public Lesson(string name, int period, int day, string where, string teacher) : base(name)
         {
-            return new SubjectUIElement(this)
+            Period = period; Day = day; Location = where; Teacher = teacher;
+        }
+
+        public SubjectUIElement GenerateUIElement()
+        {
+            return new SubjectUIElement(this);
+            // We have a method that generates UI elements rather than a simple property because this means that
+            // the UI elements are not tied to any given instance of a Lesson. This could be helpful if we were to
+            // implement multiple ways to view a timetable, for instance.
         }
     }
 
@@ -61,8 +63,8 @@ namespace DigitalTimetable
 
     class SubjectUIElement
     {
-        public Rectangle BG { get; set; }
-        public Label Text { get; set; }
+        
+        public Label MainLabel { get; set; }
         public StackLayout Backbone { get; private set; }
 
         public Lesson Parent { get; set; }
@@ -70,8 +72,8 @@ namespace DigitalTimetable
         public SubjectUIElement(Lesson parent)
         {
             Parent = parent;
-            Text.Text = parent.Abbreviation;
-
+            MainLabel.Text = parent.Abbreviation;
+            Backbone.BackgroundColor = parent.Abbreviation.GetSubjColor();
         }
     }
 }
--- DigitalTimetable/DigitalTimetable/Statics.cs
+++ DigitalTimetable/DigitalTimetable/Statics.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace DigitalTimetable
 {
     static class StaticMethods
     {
-        public static string SubjToAbbr = "([A-Z])([a-z]+$)?";
+        public static char[] alpha = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ".ToCharArray();
 
         public static string GetSubjAbbreviation(this string s)
         {
@@ -19,6 +20,36 @@ namespace DigitalTimetable
 
             
             return output.Substring(0, 3);
+        }
+
+        public static Color GetSubjColor(this string s)
+        {
+            if (s.Length > 3)
+            {
+                throw new FormatException($"String passed to GetSubjColour() was too long. Min: 3 characters, given string was {s.Length} characters long.");
+                return new Color(0, 0, 0); // in case, for some reason, I'm handling that exception.
+            }
+            
+
+            int indexR = Array.IndexOf(alpha, s[0]);
+            int indexG = Array.IndexOf(alpha, s[1]);
+            int indexB = Array.IndexOf(alpha, s[2]);
+
+            int R = (indexR * 5).Clamp(0, 255); // Our array contains 52 characters. Colors can be from 0 to 255. To get the full range, it's best to multiply by 5.
+                                                // However, as 52*5 is 260, we need some clamping. While we're doing that, we might as well refrain from it being negative too.
+            int G = (indexG * 5).Clamp(0, 255);
+            int B = (indexB * 5).Clamp(0, 255);
+
+            Color output = new Color(R, G, B); // whew, we don't have to convert them to bytes like in WPF...
+            return output;
+        }
+
+        public static int Clamp(this int i, int max, int min)
+        {
+            int limitTop = Math.Min(i, max); // not like the film format though
+            int limitBottom = Math.Max(i, min);
+
+            return limitBottom;
         }
     }
 }
